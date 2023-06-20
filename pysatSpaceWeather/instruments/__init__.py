7c4911eb8cdf2feef49c6f45e7480f927dd72102
sespinoza08 <sespino103@gmail.com>
2023-06-20 17:01:44 -0500

    updated init

--- pysatSpaceWeather/instruments/__init__.py
+++ pysatSpaceWeather/instruments/__init__.py
@@ -1,6 +1,6 @@
 from pysatSpaceWeather.instruments import methods  # noqa F401
 
-__all__ = ['ace_epam', 'ace_mag', 'ace_sis', 'ace_swepam',
+__all__ = ['ace_epam', 'ace_mag', 'ace_sis', 'ace_swepam', 'norp',
            'sw_dst', 'sw_f107', 'sw_kp', 'sw_mgii']
 
 for inst in __all__:
